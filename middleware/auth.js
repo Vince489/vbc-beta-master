ab75294f3d453dd65c8cef43b410dd985d15ced0
Vince489 <vincentcornelius489@gmail.com>
2024-04-22 13:12:39 -0400

    dashboard redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,19 @@
-import { useAuthStore } from "~/stores/authStore.js";
-
 export default defineNuxtRouteMiddleware((to, _from) => {
     const authStore = useAuthStore();
-
+  
     // Check if the user is already authenticated
     if (authStore.isAuthenticated) {
         // User is authenticated, allow navigation
         return;
     }
-
+  
     // Check if the user is trying to access the login page
     if (to.path === '/login') {
         // User is already on the login page, allow navigation
         return;
     }
-
-    // User is not authenticated and not on the login page,
-    // redirect them to the login page
-    return navigateTo(to, '/login');
-});
+  
+    // User is not authenticated, redirect them to the login page
+    return '/login';
+  });
+  
