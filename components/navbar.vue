436b85a8af1d73eb935b690a863ca3fcd78078e3
Vince489 <vincentcornelius489@gmail.com>
2024-04-25 11:24:29 -0400

    fixed

--- components/navbar.vue
+++ components/navbar.vue
@@ -7,9 +7,7 @@
           <div class="flex space-x-4">
             <!-- logo -->
             <div>
-              <nuxt-link
-                to="/"
-                class="flex items-center py-3 px-3 text-gray-300">
+              <nuxt-link to="/" class="flex items-center py-3 px-3 text-gray-300">
                 <span class="text-3xl font-bold">VBC</span>
               </nuxt-link>
             </div>
@@ -17,74 +15,64 @@
             <!-- primary nav-->
             <div class="hidden md:flex items-center space-x-3">
               <nuxt-link to="/fighters" class="font-semibold py-3 px-3 text-lg text-gray-100">FytRec</nuxt-link>
-              <!-- <nuxt-link to="/ebca" class="font-semibold py-3 px-3 text-lg text-gray-100">EBCA</nuxt-link>
-              <nuxt-link to="#/promoters" class="font-semibold py-3 text-lg px-3 text-gray-100">Promoters</nuxt-link> -->
               <nuxt-link to="/about" class="font-semibold py-3 px-3 text-lg text-gray-100">About</nuxt-link>
-              <!-- <nuxt-link to="#/contact" class="font-semibold py-3 px-3 text-lg text-gray-100">Contact</nuxt-link> -->
-              <nuxt-link v-if="isAuthenticated" to="/dashboard" class="font-semibold py-3 px-3 text-lg text-gray-100">Dashboard</nuxt-link>
             </div>
           </div>
 
           <!-- Second nav -->
           <div class="hidden md:flex items-center space-x-3">
             <template v-if="isAuthenticated">
-              <nuxt-link @click="logout" class="btn">Logout</nuxt-link>
+              <button @click="logout" class="btn">Logout</button>
             </template>
             <template v-else>
-              <nuxt-link to="login" class="font-semibold py-1 px-3 text-gray-100">Login</nuxt-link>
-              <nuxt-link to="signup" class="btn">Sign up</nuxt-link>
+              <nuxt-link to="/login" class="font-semibold py-1 px-3 text-gray-100">Login</nuxt-link>
+              <nuxt-link to="/signup" class="btn">Sign up</nuxt-link>
             </template>
           </div>
 
           <!-- mobile button-->
-          <div id="menu-btn" class="text-gray-100 md:hidden flex items-center">
-    <button @click="toggleMenu" class="mobile-menu-button border border-gray-300 rounded-md p-1">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-        </svg>
-    </button>
-</div>
+          <div id="menu-btn" class="text-gray-100 md:hidden flex items-center space-x-2">
+            <nuxt-link to="/login" class="text-gray-100 font-semibold border border-gray-100 py-1 px-2 rounded-lg hover:bg-gray-100 hover:text-gray-900">Sign in</nuxt-link>
+
+            <button @click="toggleMenu" class="mobile-menu-button border border-gray-300 rounded-md p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </div>
 
         </div>
       </div>
 
-  <!--mobile menu-->
-  <div v-if="toggle_menu" id="mobile-menu" class="bg-gray-700 mobile-menu md:hidden">
-    <nuxt-link v-if="isAuthenticated" to="/dashboard" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">Dashboard</nuxt-link>
-    <nuxt-link to="/fighters" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">FytRec</nuxt-link>
-    <nuxt-link to="/about" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">About</nuxt-link>
-    <button v-if="isAuthenticated" @click="logout" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">Logout</button>
-    <template v-else>
-      <nuxt-link to="/login" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">Login</nuxt-link>
-      <nuxt-link to="/signup" class="text-gray-100 block py-3 px-4 hover:bg-gray-600">Sign up</nuxt-link>
-    </template>
-  </div>
-
+      <!-- mobile menu-->
+      <div :class="{ 'transform translate-x-0': toggle_menu, 'transform translate-x-full': !toggle_menu }" id="mobile-menu" class="bg-gray-700 mobile-menu md:hidden transition-transform duration-500 fixed right-0 top-0 h-full w-64 overflow-y-auto">
+        <Sidebar />
+      </div>
     </nav>
   </div>
 </template>
 
 <script setup>
-import {useAuthStore} from "~/stores/authStore.js";
-import {ref, onMounted} from 'vue';
+import { useAuthStore } from "~/stores/authStore.js";
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
 const authStore = useAuthStore()
 const isAuthenticated = computed(() => authStore.isAuthenticated);
-const router = useRouter(); // Import useRouter function
+const router = useRouter();
 
 let toggle_menu = ref(false);
-
 
 onMounted(() => {
   window.addEventListener("click", function (e) {
--- pages/log.vue
+++ pages/log.vue
@@ -2,71 +2,69 @@
   <div class="flex justify-center items-center mt-32">
     <div class="w-full max-w-sm">
       <div>
-        <h1 class="p-10">Login</h1>
+        <h1 class="pb-6 text-4xl">Login</h1>
       </div>
-      <form @submit.prevent="handleSubmit" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form @submit.prevent="handleLogin" class="bg-gray-400 shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="gamerTag">
-            Gamertag
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+            Email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="gamerTag" type="text" placeholder="Gamertag" v-model="gamerTag">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" v-model="email">
+          <p class="text-red-500 text-xs italic">{{ emailError }}</p> <!-- Display email error -->
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
             Password
           </label>
           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" v-model="password">
-          <p class="hidden text-red-500 text-xs italic">Please choose a password.</p>
+          <p class="text-red-500 text-xs italic">{{ passwordError }}</p> <!-- Display password error -->
         </div>
         <div class="flex justify-center">
           <button class="btn" type="submit">
             Login
           </button>
         </div>
+        <p class="text-red-500 text-xs italic">{{ generalError }}</p> <!-- Display general error -->
       </form>
-      <p class="text-center text-gray-500 text-xs">
-        &copy;2023 Planet Virtron. All rights reserved.
-      </p>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      gamerTag: '',
-      password: ''
+<script setup>
+import { useAuthStore } from "~/stores/authStore.js";
+import { ref } from 'vue';
+import { navigateTo } from "#app";
+
+const authStore = useAuthStore()
+
+const email = ref('');
+const password = ref('');
+const emailError = ref('');
+const passwordError = ref('');
+const generalError = ref('');
+
+const handleLogin = async () => {
+  // Reset errors
+  emailError.value = '';
+  passwordError.value = '';
+  generalError.value = '';
+
+  try {
+    // Perform login
+    const response = await authStore.$login(email.value, password.value);
+    // Check if the login attempt was successful
+    if (response?.user?.isAuthenticated) {
+      return navigateTo("/dashboard")
     }
-  },
-  methods: {
-    async handleSubmit() {
-      try {
-        const response = await fetch('https://planet-virtron-api-production.up.railway.app/api/v1/gamer', {
-          method: 'POST',
-          mode: 'cors',
-          headers: { 'Content-Type': 'application/json' },
-          credentials: 'include',
-          body: JSON.stringify({
-            gamerTag: this.gamerTag,
-            password: this.password
-          })
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to submit form');
-        }
-
-        const responseData = await response.json();
-        sessionStorage.setItem('token', responseData.token)
-        sessionStorage.setItem('authenticatedGamer', JSON.stringify(responseData.authenticatedGamer));
-        console.log('Form submitted successfully');
-        this.$router.push('/dashboard');
-      } catch (error) {
-        console.error(error);
-      }
+  } catch (error) {
+    // Handle login error
+    if (error.message === 'Invalid email') {
+      emailError.value = error.message;
+    } else if (error.message === 'Incorrect password') {
+      passwordError.value = error.message;
+    } else {
+      generalError.value = 'Error logging in: ' + error.message;
     }
   }
-}
-
+};
 </script>
