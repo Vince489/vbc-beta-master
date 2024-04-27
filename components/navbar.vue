81b8fad685000f756469043e3f6a59e6538023dd
Vince489 <vincentcornelius489@gmail.com>
2024-04-27 13:03:27 -0400

    cf

--- components/navbar.vue
+++ components/navbar.vue
@@ -19,38 +19,45 @@
             </div>
           </div>
 
-          <!-- Second nav -->
-          <div class="hidden md:flex items-center space-x-3">
-            <template v-if="isAuthenticated">
-              <button @click="logout" class="btn">Logout</button>
-            </template>
-            <template v-else>
-              <nuxt-link to="/login" class="font-semibold py-1 px-3 text-gray-100">Login</nuxt-link>
-              <nuxt-link to="/signup" class="btn">Sign up</nuxt-link>
-            </template>
-          </div>
+          <!-- Second nav (hidden on mobile) -->
+<div class="hidden md:flex items-center space-x-3">
+  <template v-if="isAuthenticated">
+    <!-- Logout button -->
+    <button @click="logout" class="btn">Logout</button>
+  </template>
+</div>
 
-          <!-- mobile button-->
-          <div id="menu-btn" class="text-gray-100 md:hidden flex items-center space-x-2">
-            <nuxt-link to="/login" class="text-gray-100 font-semibold border border-gray-100 py-1 px-2 rounded-lg hover:bg-gray-100 hover:text-gray-900">Sign in</nuxt-link>
+<!-- Mobile button -->
+<div id="menu-btn" class="text-gray-100 md:hidden flex items-center space-x-2">
+  <template v-if="isAuthenticated">
+    <!-- Logout button -->
+    <button @click="logout" class="font-semibold border border-gray-100 py-1 px-2 rounded-lg hover:bg-gray-100 hover:text-gray-900">Logout</button>
+  </template>
+  <template v-else>
+    <!-- Sign in button -->
+    <nuxt-link to="/login" class="font-semibold border border-gray-100 py-1 px-2 rounded-lg hover:bg-gray-100 hover:text-gray-900">Sign in</nuxt-link>
+  </template>
 
-            <button @click="toggleMenu" class="mobile-menu-button border border-gray-300 rounded-md p-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            </button>
-          </div>
+  <!-- Mobile menu button -->
+  <button @click="toggleMenu" class="mobile-menu-button border border-gray-300 rounded-md p-1">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  </button>
+</div>
+
+
 
         </div>
       </div>
