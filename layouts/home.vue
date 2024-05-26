05de64cf5598ceb986f9fdd299132a8e2be32cc0
Vince489 <vincentcornelius489@gmail.com>
2024-05-25 20:45:16 -0400

    home no low nav

--- layouts/home.vue
+++ layouts/home.vue
@@ -2,12 +2,9 @@
   <div>
     <Navbar />
   </div>
-
   <div class="bg-gray-800 text-gray-200">
     <slot />
-
     <Footer />
-    <LowerNav /> <!-- Add the LowerNav component here -->
   </div>
 </template>
 
