c604a00321416a04fd35be29a0a1a48b7e2374b8
Vince489 <vincentcornelius489@gmail.com>
2024-05-25 20:51:06 -0400

    search

--- layouts/home.vue
+++ layouts/home.vue
@@ -2,9 +2,12 @@
   <div>
     <Navbar />
   </div>
+
   <div class="bg-gray-800 text-gray-200">
     <slot />
+
     <Footer />
+    <LowerNav /> <!-- Add the LowerNav component here -->
   </div>
 </template>
 
