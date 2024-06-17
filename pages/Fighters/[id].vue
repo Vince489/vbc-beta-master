725a5576836aaa236953d928819777c537922c87
Vince489 <vincentcornelius489@gmail.com>
2024-06-17 12:50:10 -0400

    blank

--- pages/Fighters/[id].vue
+++ pages/Fighters/[id].vue
@@ -9,7 +9,7 @@
 <script setup>
   const { fighters } = defineProps(['fighter'])
   const { id } = useRoute().params
-  const uri = 'https://virtron-production.up.railway.app/fighters/' + id 
+  const uri = 'https://vbc-login-production.up.railway.app/api/v1/fighter' + id 
 
   // fetch fighters
   const { data: fighter } = await useFetch(uri, { key: id })
