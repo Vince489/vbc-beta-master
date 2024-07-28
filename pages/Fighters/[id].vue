343461ec4adfd3a17dd983612e97f7525d2c87cc
Vince489 <vincentcornelius489@gmail.com>
2024-07-28 17:52:39 -0400

    gamerTag

--- pages/Fighters/[id].vue
+++ pages/Fighters/[id].vue
@@ -68,7 +68,7 @@
           <span :class="resultClass(bout.result)" class="px-2 py-1 rounded-full">{{ bout.result }}</span>
         </div>
         <div class="flex justify-between items-center">
-          <span class="text-lg font-semibold">vs. {{ bout.opponent.gamerTag }}</span>
+          <span class="text-lg font-semibold"> {{ bout.fighter.gamerTag }} vs. {{ bout.opponent.gamerTag }}</span>
           <span class="text-gray-400">{{ bout.method }} ({{ bout.round }}/12)</span>
         </div>
         <button @click="toggleDetails(bout._id)" class="text-blue-500 mt-2">Toggle Details</button>
