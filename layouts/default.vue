4e25ed649d945526bae525fdc88b502e3e0250f1
Prince Vince <vincentcornelius489@gmail.com>
2024-11-07 20:41:58 -0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,15 +7,4 @@
     <Footer />
     <LowerNav class="block md:hidden"/> 
   </div>
-<<<<<<< HEAD
 
-  <div class="pt-16 bg-gray-800 text-gray-200 min-h-screen">
-    <slot />
-  </div> 
-
-  <Footer />
-</template>
-
-=======
-</template>
->>>>>>> d79d43bcfc9256c87584f708397dfe7a0a3d55bb
