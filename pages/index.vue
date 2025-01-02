f7c54130ebce749065f500153cc22e7ace5cb509
Prince Vince <vincentcornelius489@gmail.com>
2025-01-01 21:38:58 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,95 +1,10 @@
 <template>
 
   <div class="pb-16">
-    <Hero />
+    <p>Be Back Soon</p>
   </div>
 
-  <section>
-
-    <div class="flex justify-center pb-10 ">
-      <h1 class="text-4xl font-semibold">IN THE SPOTLIGHT</h1>
-    </div>
-
-    <div>
-      <Spotlight1 />
-    </div>
-
-    <div>
-      <Spotlight2 />
-    </div>
-
-    <div>
-      <Spotlight3 />
-      <SlideOut />
-    </div>
-
-  </section>
-
-  <section>
-
-    <div class="flex justify-center pt-10 pb-10">
-      <h1 class="text-4xl font-semibold">RECENT FIGHTS</h1>
-    </div>
-
-    <div>
-      <Legacy />
-    </div>
-
-    <div>
-
-    </div>
-
-    <div>
-      <SlideOut />
-    </div>
-
-  </section>
-
-  <section>
-
-    <div class="flex justify-center pt-10 pb-10">
-      <h1 class="text-4xl font-semibold">Upcoming Events</h1>
-    </div>
-
-    <div>
-
-    </div>
-
-    <div>
-
-    </div>
-
-    <div>
-      
-    </div>
-
-  </section>
-
-  <section>
-
-    <div class="flex justify-center pt-10 pb-10">
-      <h1 class="text-4xl font-semibold">Tournaments</h1>
-    </div>
-
-    <div>
-
-    </div>
-
-    <div>
-
-    </div>
-
-    <div>
-    </div>
-
-  </section>
-
-  <div>
-    
-  </div>
-  
-
-</template>
+  </template>
 
 <script setup></script>
 <style scoped>
